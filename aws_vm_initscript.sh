08e7bd824c022bbc8830a325895de4c85a693c90
BugsyDev <bugsydev@gmail.com>
2022-09-14 11:28:50 +1200

    updated aws_vm_initscript added ohmyzsh zshrc config

--- aws_vm_initscript.sh
+++ aws_vm_initscript.sh
@@ -1,17 +1,54 @@
 #!/bin/sh
 # The purpose of this program is to auto install packag$# on the aws virtual machine
-#$packages=cowsay jq tmux
 
+# Installing the epel repository.
 sudo amazon-linux-extras install epel
 
-for package in cowsay git epel gcc zsh ohmyzsh zshrc jq tmux tldr
+# This is a for loop that will install all the packages listed in the loop.
+for package in cowsay git gcc zsh jq sl util-linux-user automake libevent-devel bison ncurses-devel
 do
         sudo yum install -y $package
         echo $package installed... | cowsay
 done
 
-# Install Rust
+# This is installing rust.
 echo Installing Rust... | cowsay
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > installrust.sh
 chmod +x installrust.sh
 ./installrust.sh -y
+
+# This is installing tmux.
+git clone https://github.com/tmux/tmux.git
+cd tmux
+./configure
+make
+sudo make install
+
+# This is installing tmux.
+cd $HOME
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+# Installing tldr.
+sudo pip3 install tldr
+
+# This is installing ohmyzsh.
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo ohmyzsh was installed | cowsay
+
+# This is downloading the passion.zsh-theme file from the github repository and placing it in the
+# ZSH_CUSTOM/themes/ directory.
+curl https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme > ZSH_CUSTOM/themes/passion.zsh-theme
+echo retrieved ohmyzsh theme file | cowsay
+
+# Downloading the .zshrc file from the github repository and placing it in the home directory.
+curl https://raw.githubusercontent.com/Bugsbugme/unitec-aws-restart/tree/main/dat > $HOME/.zshrc
+echo retrieved .zshrc | cowsay
+
+# This is installing the zsh-autosuggestions plugin.
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo zsh-autosuggestions was installed | cowsay
+
+# 
+sudo chsh -s $(which zsh) $(whoami) && zsh
