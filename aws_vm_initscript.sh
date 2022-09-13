935c2a870989801120d5943c33ff3cde6590cef7
BugsyDev <bugsydev@gmail.com>
2022-09-13 13:57:50 +1200

    fixed aws_vm_initscript

--- aws_vm_initscript.sh
+++ aws_vm_initscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # The purpose of this program is to auto install packag$# on the aws virtual machine
-$packages=cowsay jq tmux
+#$packages=cowsay jq tmux
 
-for package in $packages
+for package in cowsay jq tmux
 do
         sudo yum install -y $package
         echo $package installed... | cowsay
