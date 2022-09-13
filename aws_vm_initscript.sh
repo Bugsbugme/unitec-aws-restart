e7f70e992f96691ec8be64184f7de54a7b6c4556
BugsyDev <bugsydev@gmail.com>
2022-09-13 16:13:24 +1200

    added stuff

--- aws_vm_initscript.sh
+++ aws_vm_initscript.sh
@@ -1,12 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 # The purpose of this program is to auto install packag$# on the aws virtual machine
 #$packages=cowsay jq tmux
 
-for package in cowsay jq tmux
+sudo amazon-linux-extras install epel
+
+for package in cowsay git epel gcc zsh ohmyzsh zshrc jq tmux tldr
 do
         sudo yum install -y $package
         echo $package installed... | cowsay
 done
 
 # Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo Installing Rust... | cowsay
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > installrust.sh
+chmod +x installrust.sh
+./installrust.sh -y
