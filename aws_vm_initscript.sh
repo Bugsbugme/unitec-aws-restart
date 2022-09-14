d329e89ea28e8008e4e44fcecd7ad4993fa5fb03
BugsyDev <bugsydev@gmail.com>
2022-09-14 12:41:53 +1200

    fixed aws_vm_initscript

--- aws_vm_initscript.sh
+++ aws_vm_initscript.sh
@@ -2,11 +2,14 @@
 # The purpose of this program is to auto install packag$# on the aws virtual machine
 # To install, run this: "curl https://raw.githubusercontent.com/Bugsbugme/unitec-aws-restart/main/aws_vm_initscript.sh | sh"
 
+# Installing the cowsay package.
+sudo yum install cowsay -y
+
 # Installing the epel repository.
 sudo amazon-linux-extras install epel
 
 # This is a for loop that will install all the packages listed in the loop.
-for package in cowsay git gcc zsh jq sl util-linux-user automake libevent-devel bison ncurses-devel
+for package in git gcc zsh jq sl util-linux-user automake libevent-devel bison ncurses-devel
 do
         sudo yum install -y $package
         echo $package installed... | cowsay
@@ -34,14 +37,15 @@ cp .tmux/.tmux.conf.local .
 
 # Installing tldr.
 sudo pip3 install tldr
+echo tldr was installed | cowsay
 
 # This is installing ohmyzsh.
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && curl https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme > $ZSH_CUSTOM/themes/passion.zsh-theme
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && curl https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme > $ZSH_CUSTOM/themes/passion.zsh-theme && curl https://raw.githubusercontent.com/Bugsbugme/unitec-aws-restart/main/data/.zshrc > $HOME/.zshrc
 echo ohmyzsh was installed | cowsay
 
 # Downloading the .zshrc file from the github repository and placing it in the home directory.
-curl https://raw.githubusercontent.com/Bugsbugme/unitec-aws-restart/main/data/.zshrc > $HOME/.zshrc
-echo retrieved .zshrc | cowsay
+# curl https://raw.githubusercontent.com/Bugsbugme/unitec-aws-restart/main/data/.zshrc > $HOME/.zshrc
+# echo retrieved .zshrc | cowsay
 
 # This is installing the zsh-autosuggestions plugin.
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -53,4 +57,4 @@ echo zsh-autosuggestions was installed | cowsay
 # echo retrieved ohmyzsh theme file | cowsay
 
 # This is changing the default shell to zsh.
-sudo chsh -s $(which zsh) $(whoami) && zsh
+# sudo chsh -s $(which zsh) $(whoami) && zsh
