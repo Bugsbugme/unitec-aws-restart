bfc6d0c196df2f2b160c0b5b10fde9e995b67310
BugsyDev <bugsydev@gmail.com>
2022-09-14 22:35:30 +1200

    visual tweaks

--- aws_vm_initscript.sh
+++ aws_vm_initscript.sh
@@ -9,39 +9,40 @@ NC="\033[0m"
 # Installing the cowsay package.
 echo -e "[Installing cowsay...]\n"
 sudo yum install cowsay -y && echo Installed cowsay | cowsay
+echo
 
 # Installing the epel repository.
 echo Installing epel... | cowsay
 echo
-sudo amazon-linux-extras install epel && echo -e "${GREEN}[Installed epel]${NC}\n"
+sudo amazon-linux-extras install epel && echo -e "\n${GREEN}[Installed epel]${NC}\n"
 
 # This is a for loop that will install all the packages listed in the loop.
 for package in git gcc zsh jq sl util-linux-user automake libevent-devel bison ncurses-devel
 do
         echo Installing ${package}... | cowsay
         echo
-        sudo yum install -y $package && echo -e "${GREEN}[Installed ${package}]${NC}\n"
+        sudo yum install -y $package && echo -e "\n${GREEN}[Installed ${package}]${NC}\n"
 done
 
 # This is installing ohmyzsh.
 echo Installing ohmyzsh... | cowsay
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && echo -e "${GREEN}[Installed ohmyzsh]${NC}\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && echo -e "\n${GREEN}[Installed ohmyzsh]${NC}\n"
 
 # Downloading the .zshrc file from the github repository and placing it in the home directory.
 echo Fetching zshrc config... | cowsay
-curl https://raw.githubusercontent.com/Bugsbugme/unitec-aws-restart/main/data/.zshrc > $HOME/.zshrc && echo -e "${GREEN}[Retreived zshrc config]${NC}\n"
+curl https://raw.githubusercontent.com/Bugsbugme/unitec-aws-restart/main/data/.zshrc > $HOME/.zshrc && echo -e "\n${GREEN}[Retreived zshrc config]${NC}\n"
 
 # Set ZSH_CUSTOM path
-ZSH_CUSTOM=~/.oh-my-zsh/custom && echo -e "${GREEN}Set ZSH_CUSTOM path\n"
+ZSH_CUSTOM=~/.oh-my-zsh/custom && echo -e "${GREEN}[Set ZSH_CUSTOM path]${NC}\n"
 
 # This is downloading a ohmyzsh theme file from the github repository and placing it in the
 # ZSH_CUSTOM/themes/ directory.
 echo Fetching ohmyzsh theme... | cowsay
-curl https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme > $ZSH_CUSTOM/themes/passion.zsh-theme && echo -e "${GREEN}[Retreived ohmyzsh theme]${NC}\n"
+curl https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme > $ZSH_CUSTOM/themes/passion.zsh-theme && echo -e "\n${GREEN}[Retreived ohmyzsh theme]${NC}\n"
 
 # This is installing the zsh-autosuggestions plugin.
 echo Installing zsh-autosuggestions... | cowsay
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions && echo -e "${GREEN}[Installed zsh-autosuggestions]${NC}\n"
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions && echo -e "\n${GREEN}[Installed zsh-autosuggestions]${NC}\n"
 
 # This is changing the default shell to zsh.
 sudo chsh -s $(which zsh) $(whoami)
@@ -49,7 +50,7 @@ echo
 
 # Installing tldr.
 echo Installing tldr... | cowsay
-sudo pip3 install tldr && echo -e "${GREEN}[Installed tldr]${NC}\n"
+sudo pip3 install tldr && echo -e "\n${GREEN}[Installed tldr]${NC}\n"
 
 # This is installing rust.
 echo Installing rust... | cowsay
@@ -57,7 +58,7 @@ echo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > installrust.sh
 chmod +x installrust.sh
 ./installrust.sh -y
-echo -e "${GREEN}[Installed rust]${NC}\n"
+echo -e "\n${GREEN}[Installed rust]${NC}\n"
 
 # This is installing broot.
 echo Installing broot... | cowsay
@@ -65,7 +66,7 @@ mkdir $HOME/bin
 curl https://dystroy.org/broot/download/x86_64-unknown-linux-gnu/broot > $HOME/bin/broot
 chmod +x $HOME/bin/broot
 broot --install
-echo -e "${GREEN}[Installed broot]${NC}\n"
+echo -e "\n${GREEN}[Installed broot]${NC}\n"
 
 # This is installing tmux.
 echo Installing tmux... | cowsay
@@ -76,13 +77,16 @@ sh autogen.sh
 ./configure
 make
 sudo make install
-echo -e "${GREEN}[Installed tmux]${NC}\n"
+echo -e "\n${GREEN}[Installed tmux]${NC}\n"
 
 # This is installing ohmytmux.
+echo Installing ohmytmux... | cowsay
+echo
 cd $HOME
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
-echo -e "${GREEN}[Installed ohmytmux]${NC}\n"
+echo -e "\n${GREEN}[Installed ohmytmux]${NC}\n"
 
 echo -e "${GREEN}Initialization complete.${NC}" | cowsay
+echo
