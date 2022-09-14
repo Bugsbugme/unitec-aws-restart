c4462a6708e4621518539e1f2632114497907bb2
BugsyDev <bugsydev@gmail.com>
2022-09-14 12:16:43 +1200

    fixed aws_vm_initscript

--- aws_vm_initscript.sh
+++ aws_vm_initscript.sh
@@ -21,11 +21,12 @@ chmod +x installrust.sh
 # This is installing tmux.
 git clone https://github.com/tmux/tmux.git
 cd tmux
+sh autogen.sh
 ./configure
 make
 sudo make install
 
-# This is installing tmux.
+# This is installing ohmytmux.
 cd $HOME
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
@@ -35,8 +36,12 @@ cp .tmux/.tmux.conf.local .
 sudo pip3 install tldr
 
 # This is installing ohmyzsh.
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && curl https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme > $ZSH_CUSTOM/themes/passion.zsh-theme
 echo ohmyzsh was installed | cowsay
+
+# Downloading the .zshrc file from the github repository and placing it in the home directory.
+curl https://raw.githubusercontent.com/Bugsbugme/unitec-aws-restart/main/data/.zshrc > $HOME/.zshrc
+echo retrieved .zshrc | cowsay
 
 # This is installing the zsh-autosuggestions plugin.
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -44,12 +49,8 @@ echo zsh-autosuggestions was installed | cowsay
 
 # This is downloading the passion.zsh-theme file from the github repository and placing it in the
 # ZSH_CUSTOM/themes/ directory.
-curl https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme > $ZSH_CUSTOM/themes/passion.zsh-theme
-echo retrieved ohmyzsh theme file | cowsay
-
-# Downloading the .zshrc file from the github repository and placing it in the home directory.
-curl https://raw.githubusercontent.com/Bugsbugme/unitec-aws-restart/main/data/.zshrc > $HOME/.zshrc
-echo retrieved .zshrc | cowsay
+# curl https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme > $ZSH_CUSTOM/themes/passion.zsh-theme
+# echo retrieved ohmyzsh theme file | cowsay
 
 # This is changing the default shell to zsh.
 sudo chsh -s $(which zsh) $(whoami) && zsh
