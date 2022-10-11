ef1f96e0bdab3b8ed468c518d313961a34dde7f9
BugsyDev <bugsydev@gmail.com>
2022-10-12 00:12:30 +1300

    .

--- configure_docker.sh
+++ configure_docker.sh
@@ -2,7 +2,7 @@
 
 # This is configuring Docker to work with the ec2-user acoount
 echo Configuring Docker... | cowsay
-sudo usermod -a -G docker ec2-user | sh && newgrp docker | sh && sudo systemctl start docker.service | sh && echo -e "\n${GREEN}[Configured Docker]${NC}\n"
-
-echo -e "${GREEN}Initialization complete.${NC}" | cowsay
-echo
+sudo usermod -a -G docker ec2-user && newgrp docker
+sudo systemctl enable docker.service && sudo systemctl start docker.service
+groups
+echo -e "\n${GREEN}[Configured Docker]${NC}\n"
