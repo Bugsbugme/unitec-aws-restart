1fb6a4c0375194bf00a31cb9b456bcacd880cb04
BugsyDev <bugsydev@gmail.com>
2022-10-11 23:46:29 +1300

    .

--- aws_vm_initscript.sh
+++ aws_vm_initscript.sh
@@ -97,7 +97,10 @@ echo
 
 # This is configuring Docker to work with the ec2-user acoount
 echo Configuring Docker... | cowsay
-sudo usermod -a -G docker ec2-user | sh && newgrp docker | sh && sudo systemctl start docker.service | sh && echo -e "\n${GREEN}[Configured Docker]${NC}\n"
+sudo usermod -a -G docker ec2-user && newgrp docker
+sudo systemctl enable docker.service && sudo systemctl start docker.service
+groups
+echo -e "\n${GREEN}[Configured Docker]${NC}\n"
 
 echo -e "${GREEN}Initialization complete.${NC}" | cowsay
 echo
