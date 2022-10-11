a6e82282ba49d40264ff5aab3f4243aaccfd85f0
BugsyDev <bugsydev@gmail.com>
2022-10-11 19:42:14 +1300

    .

--- install_zsh.sh
+++ install_zsh.sh
@@ -11,48 +11,39 @@ echo -e "\n[Installing cowsay...]\n"
 sudo yum install cowsay -y && echo -e "${GREEN}Installed Cowsay${NC}" | cowsay
 echo
 
-echo Installing util-linux-user... | cowsay
+# This is installing the packages util-linux-user, git, and zsh.
+# util-linux-user is needed for the chsh command.
+for package in util-linux-user git zsh
+do
+        echo Installing ${package}... | cowsay
+        echo
+        sudo yum install -y $package && echo -e "\n${GREEN}[Installed ${package}]${NC}\n"
+done
+
+# This is installing ohmyzsh.
+echo Installing Oh My Zsh... | cowsay
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && echo -e "\n${GREEN}[Installed Oh My Zsh]${NC}\n"
+
+# Downloading the .zshrc file from the github repository and placing it in the home directory.
+echo Fetching zshrc config... | cowsay
+curl https://raw.githubusercontent.com/Bugsbugme/unitec-aws-restart/main/data/.zshrc > $HOME/.zshrc && echo -e "\n${GREEN}[Retreived zshrc config]${NC}\n"
+
+# This is downloading an ohmyzsh theme file from the github repository and placing it in the ZSH_CUSTOM/themes/ directory.
+echo Fetching Oh My Zsh theme... | cowsay
 echo
-sudo yum install util-linux-user -y && echo -e "\n${GREEN}[Installed util-linux-user]${NC}\n"
+ZSH_CUSTOM=~/.oh-my-zsh/custom
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1 && echo -e "\n${GREEN}[Retreived ohmyzsh theme]${NC}\n"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# This is installing the zsh-autosuggestions plugin.
+echo Installing zsh-autosuggestions... | cowsay
 echo
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions && echo -e "\n${GREEN}[Installed zsh-autosuggestions]${NC}\n"
 
-# This is installing the epel repository and then echoing the message that it was installed.
-echo Installing Epel Repsitory... | cowsay
+# This is installing the zsh-syntax-highlighting plugin.
+echo Installing zsh-syntax-highlighting... | cowsay
 echo
-sudo amazon-linux-extras install epel && echo -e "\n${GREEN}[Installed Epel Repository]${NC}\n"
-echo
-
-# This is installing the ZShell package and then echoing the message that it was installed.
-echo Installing ZShell... | cowsay
-echo
-sudo yum install -y zsh && echo -e "\n${GREEN}[Installed ZShell]${NC}\n"
-echo
-
-# # This is installing ohmyzsh.
-# echo Installing Oh My Zsh... | cowsay
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && echo -e "\n${GREEN}[Installed Oh My Zsh]${NC}\n"
-
-# # Downloading the .zshrc file from the github repository and placing it in the home directory.
-# echo Fetching zshrc config... | cowsay
-# curl https://raw.githubusercontent.com/Bugsbugme/unitec-aws-restart/main/data/.zshrc > $HOME/.zshrc && echo -e "\n${GREEN}[Retreived zshrc config]${NC}\n"
-
-# # This is downloading an ohmyzsh theme file from the github repository and placing it in the
-# # ZSH_CUSTOM/themes/ directory.
-# echo Fetching Oh My Zsh theme... | cowsay
-# echo
-# ZSH_CUSTOM=~/.oh-my-zsh/custom
-# git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1 && echo -e "\n${GREEN}[Retreived ohmyzsh theme]${NC}\n"
-# ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-# # This is installing the zsh-autosuggestions plugin.
-# echo Installing zsh-autosuggestions... | cowsay
-# echo
-# git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions && echo -e "\n${GREEN}[Installed zsh-autosuggestions]${NC}\n"
-
-# # This is installing the zsh-syntax-highlighting plugin.
-# echo Installing zsh-syntax-highlighting... | cowsay
-# echo
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting && echo -e "\n${GREEN}[Installed zsh-syntax-highlighting]${NC}\n"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting && echo -e "\n${GREEN}[Installed zsh-syntax-highlighting]${NC}\n"
 
 # This is changing the default shell to zsh.
 sudo chsh -s "$zsh" && sudo chsh -s "$zsh" "$USER" && exec zsh -l
